fc37eef1f4779477c63d45d2d214bf92de0fac85
Agustin Bravo Rubio <abravo@itx.com>
2019-11-06 11:51:35 -0300

    added search and filter functionality

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 //PRODUCTOS------------------------//
-// Init Isotope
-var $grid = $('.grid').isotope({
-  itemSelector: '.card',
-  masonry: {
-    gutter: 15,
-  }
-});
+//SEARCH AND FILTER
+$(document).ready(function(){
+  var $btns = $(document).on("click", ".filters li a", function(){
+    if (this.id == 'todos') {
+      $('#grid > .grid-item').fadeIn(500);
+    } else {
+      var $el = $('.' + this.id).fadeIn(500);
+      $('#grid > .grid-item').not($el).hide();
+    }
+    $btns.removeClass('active');
+    $(this).addClass('active');
+  })
 
-// filter items on button click
-$(document).on("click", ".filters li a", function(){
-  var selector = $(this).attr('data-filter');
-  $grid.isotope({ filter: selector });
-  return false;
-});
+  var $search = $(document).on("input", "#search", function(){
+    $btns.removeClass('active');
+    var matcher = new RegExp($(this).val(), 'gi');
+    $('#grid > .grid-item').show().not(function(){
+        return matcher.test($(this).find('.card-title').text())
+    }).hide();
+  })
+})
