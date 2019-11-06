a3760edede52027851ecebc1e073fb8a2345a89a
Agustin Bravo Rubio <abravo@itx.com>
2019-11-05 23:28:28 -0300

    added filter in products

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+//PRODUCTOS------------------------//
+// Init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.card',
+  masonry: {
+    gutter: 15,
+  }
+});
 
+// filter items on button click
+$(document).on("click", ".filters li a", function(){
+  var selector = $(this).attr('data-filter');
+  $grid.isotope({ filter: selector });
+  return false;
+});
