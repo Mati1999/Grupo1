a784a54794d921ca5016e0662923a1d01185391d
Agustin Bravo Rubio <abravo@itx.com>
2019-11-14 14:58:12 -0300

    added carrito

--- js/app.js
+++ js/app.js
@@ -1,22 +1,32 @@
-//PRODUCTOS------------------------//
-//SEARCH AND FILTER
-$(document).ready(function(){
-  var $btns = $(document).on("click", ".filters li a", function(){
-    if (this.id == 'todos') {
-      $('#grid > .grid-item').fadeIn(500);
-    } else {
-      var $el = $('.' + this.id).fadeIn(500);
-      $('#grid > .grid-item').not($el).hide();
-    }
-    $btns.removeClass('active');
-    $(this).addClass('active');
-  })
+$( window ).on( "load", function() {
+  
+  //PRODUCTOS------------------------//
+  //SEARCH AND FILTER
+  $(document).ready(function(){
+    var $btns = $(document).on("click", ".filters li a", function(){
+      if (this.id == 'todos') {
+        $('#grid > .grid-item').fadeIn(500);
+      } else {
+        var $el = $('.' + this.id).fadeIn(500);
+        $('#grid > .grid-item').not($el).hide();
+      }
+      $btns.removeClass('active');
+      $(this).addClass('active');
+    })
 
-  var $search = $(document).on("input", "#search", function(){
-    $btns.removeClass('active');
-    var matcher = new RegExp($(this).val(), 'gi');
-    $('#grid > .grid-item').show().not(function(){
-        return matcher.test($(this).find('.card-title').text())
-    }).hide();
-  })
+    var $search = $(document).on("input", "#search", function(){
+      $btns.removeClass('active');
+      var matcher = new RegExp($(this).val(), 'gi');
+      $('#grid > .grid-item').show().not(function(){
+          return matcher.test($(this).find('.card-title').text())
+      }).hide();
+    })
+  });
+
+  //CARGAR PUSHBAR CARRITO
+  var pushbar = new Pushbar({
+    blur:true,
+    overlay:true
+  });
+
 });
--- js/commonComponents.js
+++ js/commonComponents.js
@@ -41,7 +41,7 @@ class CustomHeader extends HTMLElement {
         <div class="navbar-collapse navbar-user">
           <ul class="navbar-nav ml-auto">
               <li class="nav-item">
-                <a class="nav-link" href="#" data-toggle="modal" data-target="#">
+                <a class="nav-link" href="#" data-pushbar-target="pushbar-carrito">
                   <ion-icon name="cart" size="large"></ion-icon>
                 </a>
               </li>
@@ -54,6 +54,39 @@ class CustomHeader extends HTMLElement {
         </div>
 
       </nav>
+
+      <!-- CONTENIDO DE PUSHBAR CARRITO ----------------------------------------------->
+      <div data-pushbar-id="pushbar-carrito" class="pushbar from_right pushbar-carrito">
+        <div class="btn-cerrar derecha">
+          <button data-pushbar-close> <ion-icon name="close"></ion-icon></button>
+        </div>
+
+        <p class="tituloCarrito">Carrito de Compras</p>
+
+        <div class="item">
+          <img src="../img/bebida-1.jpeg" class="card-img-top" alt="...">
+          <div class="textos">
+            <p class="nombre-item">Combo N°1</p>
+            <p>$300</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="../img/bebida-2.jpg" class="card-img-top" alt="...">
+          <div class="textos">
+            <p class="nombre-item">Combo N°2</p>
+            <p>$600</p>
+          </div>
+        </div>
+
+        <div class="total">
+          <p class="precio-total">Total:</p>
+          <p>$900</p>
+        </div>
+
+      </div>
+      <!------------------------------------------------------------------------------->
+
     </header>
 
     <!-- LOGIN AND SIGNUP -->
@@ -197,3 +230,122 @@ class CustomFooter extends HTMLElement {
 };
 
 window.customElements.define('custom-footer', CustomFooter);
+
+//CARRITO-----------------------//
+//PUSHBAR
+class Pushbar {
+  constructor(config = { overlay: true, blur: false }) {
+    this.activeBar = null;
+    this.overlay = false;
+
+    if (config.overlay) {
+      this.overlay = document.createElement('div');
+      this.overlay.classList.add('pushbar_overlay');
+      document.querySelector('body').appendChild(this.overlay);
+    }
+
+    if (config.blur) {
+      const mainContent = document.querySelector('.pushbar_main_content');
+      if (mainContent) {
+        mainContent.classList.add('pushbar_blur');
+      }
+    }
+
+    this.bindEvents();
+  }
+
+  get opened() {
+    const { activeBar } = this;
+    return Boolean(activeBar instanceof HTMLElement && activeBar.classList.contains('opened'));
+  }
+
+  get activeBarId() {
+    const { activeBar } = this;
+    return activeBar instanceof HTMLElement && activeBar.getAttribute('data-pushbar-id');
+  }
+
+  static dispatchOpen(pushbar) {
+    const event = new CustomEvent('pushbar_opening', { bubbles: true, detail: { pushbar } });
+    pushbar.dispatchEvent(event);
+  }
+
+  static dispatchClose(pushbar) {
+    const event = new CustomEvent('pushbar_closing', { bubbles: true, detail: { pushbar } });
+    pushbar.dispatchEvent(event);
+  }
+
+  static findElementById(pushbarId) {
+    return document.querySelector(`[data-pushbar-id="${pushbarId}"]`);
+  }
+
+  handleOpenEvent(e) {
+    e.preventDefault();
+    const pushbarId = e.currentTarget.getAttribute('data-pushbar-target');
+    if (pushbarId) {
+      this.open(pushbarId);
+    }
+  }
+
+  handleCloseEvent(e) {
+    e.preventDefault();
+    this.close();
+  }
+
+  handleKeyEvent(e) {
+    if (this.opened && e.keyCode === 27) {
+      this.close();
+    }
+  }
+
+  bindEvents() {
+    const triggers = document.querySelectorAll('[data-pushbar-target]');
+    const closers = document.querySelectorAll('[data-pushbar-close]');
+
+    triggers.forEach(trigger => trigger.addEventListener('click', e => this.handleOpenEvent(e), false));
+    closers.forEach(closer => closer.addEventListener('click', e => this.handleCloseEvent(e), false));
+
+    if (this.overlay) {
+      this.overlay.addEventListener('click', e => this.handleCloseEvent(e), false);
+    }
+    document.addEventListener('keyup', e => this.handleKeyEvent(e));
+  }
+
+  open(pushbarId) {
+    // Current bar is already opened
+    if (String(pushbarId) === this.activeBarId && this.opened) {
+      return;
+    }
+
+    // Get new pushbar target
+    const pushbar = Pushbar.findElementById(pushbarId);
+
+    if (!pushbar) return;
+
+    // Close active bar (if exists)
+    if (this.opened) {
+      this.close();
+    }
+
+    Pushbar.dispatchOpen(pushbar);
+    pushbar.classList.add('opened');
+
+    const Root = document.querySelector('html');
+    Root.classList.add('pushbar_locked');
+    Root.setAttribute('pushbar', pushbarId);
+    this.activeBar = pushbar;
+  }
+
+  close() {
+    const { activeBar } = this;
+    if (!activeBar) return;
+
+    Pushbar.dispatchClose(activeBar);
+    activeBar.classList.remove('opened');
+
+    const Root = document.querySelector('html');
+    Root.classList.remove('pushbar_locked');
+    Root.removeAttribute('pushbar');
+
+    this.activeBar = null;
+  }
+};
