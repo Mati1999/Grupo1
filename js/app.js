f22153942804a6b92a152e918e0ba8b6c4681b8f
Agustin Bravo Rubio <abravo@itx.com>
2019-11-15 12:20:39 -0300

    agregar/quitar productos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 $( window ).on( "load", function() {
-  
-  //PRODUCTOS------------------------//
+
   //SEARCH AND FILTER
   $(document).ready(function(){
     var $btns = $(document).on("click", ".filters li a", function(){
@@ -23,10 +22,186 @@ $( window ).on( "load", function() {
     })
   });
 
-  //CARGAR PUSHBAR CARRITO
+  //CARRITO-----------------------//
+  //PUSHBAR
+  class Pushbar {
+    constructor(config = { overlay: true, blur: false }) {
+      this.activeBar = null;
+      this.overlay = false;
+
+      if (config.overlay) {
+        this.overlay = document.createElement('div');
+        this.overlay.classList.add('pushbar_overlay');
+        document.querySelector('body').appendChild(this.overlay);
+      }
+
+      if (config.blur) {
+        const mainContent = document.querySelector('.pushbar_main_content');
+        if (mainContent) {
+          mainContent.classList.add('pushbar_blur');
+        }
+      }
+
+      this.bindEvents();
+    }
+
+    get opened() {
+      const { activeBar } = this;
+      return Boolean(activeBar instanceof HTMLElement && activeBar.classList.contains('opened'));
+    }
+
+    get activeBarId() {
+      const { activeBar } = this;
+      return activeBar instanceof HTMLElement && activeBar.getAttribute('data-pushbar-id');
+    }
+
+    static dispatchOpen(pushbar) {
+      const event = new CustomEvent('pushbar_opening', { bubbles: true, detail: { pushbar } });
+      pushbar.dispatchEvent(event);
+    }
+
+    static dispatchClose(pushbar) {
+      const event = new CustomEvent('pushbar_closing', { bubbles: true, detail: { pushbar } });
+      pushbar.dispatchEvent(event);
+    }
+
+    static findElementById(pushbarId) {
+      return document.querySelector(`[data-pushbar-id="${pushbarId}"]`);
+    }
+
+    handleOpenEvent(e) {
+      e.preventDefault();
+      const pushbarId = e.currentTarget.getAttribute('data-pushbar-target');
+      if (pushbarId) {
+        this.open(pushbarId);
+      }
+    }
+
+    handleCloseEvent(e) {
+      e.preventDefault();
+      this.close();
+    }
+
+    handleKeyEvent(e) {
+      if (this.opened && e.keyCode === 27) {
+        this.close();
+      }
+    }
+
+    bindEvents() {
+      const triggers = document.querySelectorAll('[data-pushbar-target]');
+      const closers = document.querySelectorAll('[data-pushbar-close]');
+
+      triggers.forEach(trigger => trigger.addEventListener('click', e => this.handleOpenEvent(e), false));
+      closers.forEach(closer => closer.addEventListener('click', e => this.handleCloseEvent(e), false));
+
+      if (this.overlay) {
+        this.overlay.addEventListener('click', e => this.handleCloseEvent(e), false);
+      }
+      document.addEventListener('keyup', e => this.handleKeyEvent(e));
+    }
+
+    open(pushbarId) {
+      // Current bar is already opened
+      if (String(pushbarId) === this.activeBarId && this.opened) {
+        return;
+      }
+
+      // Get new pushbar target
+      const pushbar = Pushbar.findElementById(pushbarId);
+
+      if (!pushbar) return;
+
+      // Close active bar (if exists)
+      if (this.opened) {
+        this.close();
+      }
+
+      Pushbar.dispatchOpen(pushbar);
+      pushbar.classList.add('opened');
+
+      const Root = document.querySelector('html');
+      Root.classList.add('pushbar_locked');
+      Root.setAttribute('pushbar', pushbarId);
+      this.activeBar = pushbar;
+    }
+
+    close() {
+      const { activeBar } = this;
+      if (!activeBar) return;
+
+      Pushbar.dispatchClose(activeBar);
+      activeBar.classList.remove('opened');
+
+      const Root = document.querySelector('html');
+      Root.classList.remove('pushbar_locked');
+      Root.removeAttribute('pushbar');
+
+      this.activeBar = null;
+    }
+  };
+
   var pushbar = new Pushbar({
     blur:true,
     overlay:true
   });
 
+  //QUITAR PRODUCTO DEL CARRITO
+  function quitarProducto() {
+
+    var removeProductButtons = document.querySelectorAll('.card .quitar');
+    for (var i=0; i < removeProductButtons.length; i++) {
+
+      removeProductButtons[i].addEventListener('click', function(){
+
+        //Crear Item
+        item = this.closest('.card');
+        this.innerHTML = "Agregar al carrito"
+        this.classList.remove("quitar");
+        this.classList.add("agregar");
+
+        //Agregar Item a la grilla
+        document.getElementById('grid').appendChild(item);
+
+
+
+
+
+
+        agregarProducto();
+      });
+    };
+  };
+
+  //AGREGAR PRODUCTO AL CARRITO
+  function agregarProducto() {
+
+    var addProductButtons = document.querySelectorAll('.card .agregar');
+    for (var i = 0; i < addProductButtons.length; i++) {
+
+      addProductButtons[i].addEventListener('click', function(){
+
+        //Crear Item
+        item = this.closest('.card');
+        this.innerHTML = "Quitar"
+        this.classList.remove("agregar");
+        this.classList.add("quitar");
+
+        //Agregar Item al carrito
+        document.getElementById('lista-carrito').appendChild(item);
+
+        quitarProducto();
+      });
+    };
+  };
+
+  agregarProducto();
+
 });
+
+/*TO DO -----------------------------------------
+1- Leer de la base de datos y agregar cada card.
+2- Cada card debe tener un boton "Agregar al carro" y el "id"
+3- Cada boton tiene que implementar la clase "agregar-producto"
+4- Cada boton se tiene que suscribir al evento para agregar productos
+*/
--- js/commonComponents.js
+++ js/commonComponents.js
@@ -61,27 +61,15 @@ class CustomHeader extends HTMLElement {
           <button data-pushbar-close> <ion-icon name="close"></ion-icon></button>
         </div>
 
-        <p class="tituloCarrito">Carrito de Compras</p>
+        <h4 class="tituloCarrito">Carrito de Compras</h4>
 
-        <div class="item">
-          <img src="../img/bebida-1.jpeg" class="card-img-top" alt="...">
-          <div class="textos">
-            <p class="nombre-item">Combo N°1</p>
-            <p>$300</p>
-          </div>
-        </div>
+        <div class="lista-carrito" id="lista-carrito">
 
-        <div class="item">
-          <img src="../img/bebida-2.jpg" class="card-img-top" alt="...">
-          <div class="textos">
-            <p class="nombre-item">Combo N°2</p>
-            <p>$600</p>
-          </div>
         </div>
 
         <div class="total">
-          <p class="precio-total">Total:</p>
-          <p>$900</p>
+          <span class="precio-total">Total: $</span>
+          <span>0</span>
         </div>
 
       </div>
@@ -91,78 +79,78 @@ class CustomHeader extends HTMLElement {
 
     <!-- LOGIN AND SIGNUP -->
     <div class="modal fade" id="loginyrg" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-body">
+      <div class="modal-body">
 
-                  <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <ul class="nav nav-tabs card-header-tabs" role="tablist">
-                          <li class="nav-item"><a class="nav-link active" href="#signin-tab" data-toggle="tab" role="tab" aria-selected="false"><i class="czi-unlocked mr-2 mt-n1"></i>Log in</a></li>
-                          <li class="nav-item"><a class="nav-link" href="#signup-tab" data-toggle="tab" role="tab" aria-selected="true"><i class="czi-user mr-2 mt-n1"></i>Sign up</a></li>
-                        </ul>
-                        <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                      </div>
-                      <div class="modal-body tab-content py-4">
-                         <!-- LOGIN -->
-                        <form class="needs-validation tab-pane fade active show" autocomplete="off" novalidate="" id="signin-tab">
-                          <div class="form-group">
-                            <label for="si-email">Correo electrónico</label>
-                            <input class="form-control" type="email" id="si-email" placeholder="tuCorreo@ejemplo.com" required="">
-                            <div class="invalid-feedback">Profavor, ingrese un correo válido</div>
-                          </div>
-                          <div class="form-group">
-                            <label for="si-password">Contraseña</label>
-                            <div class="password-toggle">
-                              <input class="form-control" type="password" id="si-password" required="" placeholder="********">
-                              <label class="password-toggle-btn">
-                                <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Show password</span>
-                              </label>
-                            </div>
-                          </div>
-                          <div class="form-group d-flex flex-wrap justify-content-between">
-                            <div class="custom-control custom-checkbox mb-2">
-                              <input class="custom-control-input" type="checkbox" id="si-remember">
-                              <label class="custom-control-label" for="si-remember">Recordarme</label>
-                            </div><a class="font-size-sm" href="#">Olvidaste tu contraseña?</a>
-                          </div>
-                          <button class="btn btn-primary btn-block btn-shadow" type="submit">Log in</button>
-                        </form>
-                         <!-- SIGNUP -->
-                        <form class="needs-validation tab-pane fade" autocomplete="off" novalidate="" id="signup-tab">
-                          <div class="form-group">
-                            <label for="su-name">Nombre completo</label>
-                            <input class="form-control" type="text" id="su-name" placeholder="Nombre y Apellido" required="">
-                            <div class="invalid-feedback">Porfavor, ingrese su nombre completo.</div>
-                          </div>
-                          <div class="form-group">
-                            <label for="su-email">Correo electrónico</label>
-                            <input class="form-control" type="email" id="su-email" placeholder="tuCorreo@ejemplo.com" required="">
-                            <div class="invalid-feedback">Porfavor, ingrese un correo válido.</div>
-                          </div>
-                          <div class="form-group contra">
-                            <label for="su-password">Contraseña</label>
-                            <div class="password-toggle">
-                              <input class="form-control" type="password" id="su-password" required="" placeholder="********">
-                              <label class="password-toggle-btn">
-                                <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Show password</span>
-                              </label>
-                            </div>
-                          </div>
-                          <div class="form-group">
-                            <label for="su-password-confirm">Confirmar contraseña</label>
-                            <div class="password-toggle">
-                              <input class="form-control" type="password" id="su-password-confirm" required="" placeholder="********">
-                              <label class="password-toggle-btn">
-                                <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Show password</span>
-                              </label>
-                            </div>
-                          </div>
-                          <button class="btn btn-primary btn-block btn-shadow" type="submit">Sign up</button>
-                        </form>
-                      </div>
-                    </div>
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <ul class="nav nav-tabs card-header-tabs" role="tablist">
+                <li class="nav-item"><a class="nav-link active" href="#signin-tab" data-toggle="tab" role="tab" aria-selected="false"><i class="czi-unlocked mr-2 mt-n1"></i>Log in</a></li>
+                <li class="nav-item"><a class="nav-link" href="#signup-tab" data-toggle="tab" role="tab" aria-selected="true"><i class="czi-user mr-2 mt-n1"></i>Sign up</a></li>
+              </ul>
+              <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+            </div>
+            <div class="modal-body tab-content py-4">
+               <!-- LOGIN -->
+              <form class="needs-validation tab-pane fade active show" autocomplete="off" novalidate="" id="signin-tab">
+                <div class="form-group">
+                  <label for="si-email">Correo electrónico</label>
+                  <input class="form-control" type="email" id="si-email" placeholder="tuCorreo@ejemplo.com" required="">
+                  <div class="invalid-feedback">Profavor, ingrese un correo válido</div>
+                </div>
+                <div class="form-group">
+                  <label for="si-password">Contraseña</label>
+                  <div class="password-toggle">
+                    <input class="form-control" type="password" id="si-password" required="" placeholder="********">
+                    <label class="password-toggle-btn">
+                      <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Show password</span>
+                    </label>
                   </div>
+                </div>
+                <div class="form-group d-flex flex-wrap justify-content-between">
+                  <div class="custom-control custom-checkbox mb-2">
+                    <input class="custom-control-input" type="checkbox" id="si-remember">
+                    <label class="custom-control-label" for="si-remember">Recordarme</label>
+                  </div><a class="font-size-sm" href="#">Olvidaste tu contraseña?</a>
+                </div>
+                <button class="btn btn-primary btn-block btn-shadow" type="submit">Log in</button>
+              </form>
+               <!-- SIGNUP -->
+              <form class="needs-validation tab-pane fade" autocomplete="off" novalidate="" id="signup-tab">
+                <div class="form-group">
+                  <label for="su-name">Nombre completo</label>
+                  <input class="form-control" type="text" id="su-name" placeholder="Nombre y Apellido" required="">
+                  <div class="invalid-feedback">Porfavor, ingrese su nombre completo.</div>
+                </div>
+                <div class="form-group">
+                  <label for="su-email">Correo electrónico</label>
+                  <input class="form-control" type="email" id="su-email" placeholder="tuCorreo@ejemplo.com" required="">
+                  <div class="invalid-feedback">Porfavor, ingrese un correo válido.</div>
+                </div>
+                <div class="form-group contra">
+                  <label for="su-password">Contraseña</label>
+                  <div class="password-toggle">
+                    <input class="form-control" type="password" id="su-password" required="" placeholder="********">
+                    <label class="password-toggle-btn">
+                      <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Show password</span>
+                    </label>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="su-password-confirm">Confirmar contraseña</label>
+                  <div class="password-toggle">
+                    <input class="form-control" type="password" id="su-password-confirm" required="" placeholder="********">
+                    <label class="password-toggle-btn">
+                      <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Show password</span>
+                    </label>
+                  </div>
+                </div>
+                <button class="btn btn-primary btn-block btn-shadow" type="submit">Sign up</button>
+              </form>
+            </div>
           </div>
+        </div>
+      </div>
     </div>
 
     `
@@ -230,122 +218,3 @@ class CustomFooter extends HTMLElement {
 };
 
 window.customElements.define('custom-footer', CustomFooter);
-
-//CARRITO-----------------------//
-//PUSHBAR
-class Pushbar {
-  constructor(config = { overlay: true, blur: false }) {
-    this.activeBar = null;
-    this.overlay = false;
-
-    if (config.overlay) {
-      this.overlay = document.createElement('div');
-      this.overlay.classList.add('pushbar_overlay');
-      document.querySelector('body').appendChild(this.overlay);
-    }
-
-    if (config.blur) {
-      const mainContent = document.querySelector('.pushbar_main_content');
-      if (mainContent) {
-        mainContent.classList.add('pushbar_blur');
-      }
-    }
-
-    this.bindEvents();
-  }
-
-  get opened() {
-    const { activeBar } = this;
-    return Boolean(activeBar instanceof HTMLElement && activeBar.classList.contains('opened'));
-  }
-
-  get activeBarId() {
-    const { activeBar } = this;
-    return activeBar instanceof HTMLElement && activeBar.getAttribute('data-pushbar-id');
-  }
-
-  static dispatchOpen(pushbar) {
-    const event = new CustomEvent('pushbar_opening', { bubbles: true, detail: { pushbar } });
-    pushbar.dispatchEvent(event);
-  }
-
-  static dispatchClose(pushbar) {
-    const event = new CustomEvent('pushbar_closing', { bubbles: true, detail: { pushbar } });
-    pushbar.dispatchEvent(event);
-  }
-
-  static findElementById(pushbarId) {
-    return document.querySelector(`[data-pushbar-id="${pushbarId}"]`);
-  }
-
-  handleOpenEvent(e) {
-    e.preventDefault();
-    const pushbarId = e.currentTarget.getAttribute('data-pushbar-target');
-    if (pushbarId) {
-      this.open(pushbarId);
-    }
-  }
-
-  handleCloseEvent(e) {
-    e.preventDefault();
-    this.close();
-  }
-
-  handleKeyEvent(e) {
-    if (this.opened && e.keyCode === 27) {
-      this.close();
-    }
-  }
-
-  bindEvents() {
-    const triggers = document.querySelectorAll('[data-pushbar-target]');
-    const closers = document.querySelectorAll('[data-pushbar-close]');
-
-    triggers.forEach(trigger => trigger.addEventListener('click', e => this.handleOpenEvent(e), false));
-    closers.forEach(closer => closer.addEventListener('click', e => this.handleCloseEvent(e), false));
-
-    if (this.overlay) {
-      this.overlay.addEventListener('click', e => this.handleCloseEvent(e), false);
-    }
-    document.addEventListener('keyup', e => this.handleKeyEvent(e));
-  }
-
-  open(pushbarId) {
-    // Current bar is already opened
-    if (String(pushbarId) === this.activeBarId && this.opened) {
-      return;
-    }
-
-    // Get new pushbar target
-    const pushbar = Pushbar.findElementById(pushbarId);
-
-    if (!pushbar) return;
-
-    // Close active bar (if exists)
-    if (this.opened) {
-      this.close();
-    }
-
-    Pushbar.dispatchOpen(pushbar);
-    pushbar.classList.add('opened');
-
-    const Root = document.querySelector('html');
-    Root.classList.add('pushbar_locked');
-    Root.setAttribute('pushbar', pushbarId);
-    this.activeBar = pushbar;
-  }
-
-  close() {
-    const { activeBar } = this;
-    if (!activeBar) return;
-
-    Pushbar.dispatchClose(activeBar);
-    activeBar.classList.remove('opened');
-
-    const Root = document.querySelector('html');
-    Root.classList.remove('pushbar_locked');
-    Root.removeAttribute('pushbar');
-
-    this.activeBar = null;
-  }
-};
