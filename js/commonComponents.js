fae83ae6a9d244a2ee19146342db020f535ddbb4
Matias Aguilera <aguileramati50@gmail.com>
2019-11-29 13:40:15 -0300

    perfil de usuario

--- js/commonComponents.js
+++ js/commonComponents.js
@@ -6,6 +6,7 @@ class CustomHeader extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
+
     <header class="common-header">
       <nav class="navbar navbar-expand-lg ">
         <ion-icon name="beer" size="large" style="color:white;"></ion-icon>
@@ -82,26 +83,33 @@ class CustomHeader extends HTMLElement {
     <div class="modal fade" id="loginyrg" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-body animated slideInDown" style="padding: 0;">
         <div class="modal-dialog modal-dialog-centered" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
+          <div class="modal-content decoracionHeaderLogIn decoracionBodyLogIn">
+            <div class="modal-header bg-dark decoracionHeaderLogIn decoracionHeaderLogIn1">
               <ul class="nav nav-tabs card-header-tabs" role="tablist">
-                <li class="nav-item"><a class="nav-link active" href="#signin-tab" data-toggle="tab" role="tab" aria-selected="false"><i class="czi-unlocked mr-2 mt-n1"></i>Log in</a></li>
-                <li class="nav-item"><a class="nav-link" href="#signup-tab" data-toggle="tab" role="tab" aria-selected="true"><i class="czi-user mr-2 mt-n1"></i>Registráte</a></li>
+                <li class="nav-item"><a class="nav-link active botonHeader" href="#signin-tab" data-toggle="tab" role="tab" aria-selected="false"><i class="czi-unlocked mr-2 mt-n1"></i>Log in</a></li>
+                <li class="nav-item"><a class="nav-link botonHeader" href="#signup-tab" data-toggle="tab" role="tab" aria-selected="true"><i class="czi-user mr-2 mt-n1"></i>Registráte</a></li>
               </ul>
-              <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+              <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span class="botonDeCierre" aria-hidden="true">×</span></button>
             </div>
+
                <!-- LOGIN -->
-            <div class="modal-body tab-content">
+
+            <div class="modal-body tab-content bg-dark decoracionBodyLogIn">
               <form class="needs-validation tab-pane fade active show" autocomplete="off" novalidate="" id="signin-tab">
                 <div class="form-group">
-                  <label for="si-email">Correo electrónico</label>
-                  <input class="form-control" type="email" id="si-email" placeholder="tuCorreo@ejemplo.com" required="">
+                  <label class="colorFuente" for="si-email">Correo electrónico</label>
+                  <div class="d-flex">
+                  <div class="bg-white iconos"><img src="../img/iconmonstr-user-5-32.png" width="80%" height="80%" alt="..."></div>
+                  <input class="form-control placeHolder" type="email" id="si-email" placeholder="tuCorreo@ejemplo.com" required="">
                   <div class="invalid-feedback">Profavor, ingrese un correo válido</div>
+                  </div>
                 </div>
                 <div class="form-group">
-                  <label for="si-password">Contraseña</label>
-                  <div class="password-toggle">
-                    <input class="form-control" type="password" id="si-password" required="" placeholder="********">
+                  <label class="colorFuente" for="si-password">Contraseña</label>
+                  <div class="password-toggle d-flex">
+                  <div class="bg-white iconos"><img src="../img/iconmonstr-email-12-32.png" width="80%" height="80%" alt="..."></div>
+                    <input class="form-control placeHolder" type="password" id="si-password" required="" placeholder="********">
                     <label class="password-toggle-btn">
                       <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Show password</span>
                     </label>
@@ -110,39 +118,41 @@ class CustomHeader extends HTMLElement {
                 <div class="form-group d-flex flex-wrap justify-content-between">
                   <div class="custom-control custom-checkbox mb-2">
                     <input class="custom-control-input" type="checkbox" id="si-remember">
-                    <label class="custom-control-label" for="si-remember">Recordarme</label>
-                  </div><a class="font-size-sm" href="#">Olvidaste tu contraseña?</a>
+                    <label class="custom-control-label colorFuente" for="si-remember">Recordarme</label>
+                  </div><a class="font-size-sm decoracion" href="#">Olvidaste tu contraseña?</a>
                 </div>
                 <div class="text-center p-2">
-                  <button class="btn btn-outline-dark btn-lg" type="submit">Log in</button>
+                  <button class="botonLogSign btn-lg" type="submit">Log in</button>
                 </div>
               </form>
+
                <!-- SIGNUP -->
+
               <form class="needs-validation tab-pane fade" autocomplete="off" novalidate="" id="signup-tab">
               <div class="form-row">
                 <div class="form-group col-lg-6 col-12 pr-0 pl-0">
-                  <label for="su-name">Nombre</label>
+                  <label class="colorFuente" for="su-name">Nombre</label>
                   <input class="form-control" type="text" id="su-name" placeholder="Nombre" required="">
                   <div class="invalid-feedback">Porfavor, ingrese su nombre.</div>
                 </div>
-                <div class="form-group col-lg-6 col-12 pr-0">
-                  <label for="su-last_name">Apellido</label>
+                <div class="form-group col-lg-6 col-12 pl-1">
+                  <label class="colorFuente" for="su-last_name">Apellido</label>
                   <input class="form-control" type="text" id="su-name" placeholder="Apellido" required="">
                   <div class="invalid-feedback">Porfavor, ingrese su apellido.</div>
                 </div>
                 </div>
                 <div class="form-group">
-                  <label for="su-email">Fecha de nacimiento</label>
+                  <label class="colorFuente" for="su-email">Fecha de nacimiento</label>
                   <input class="form-control" type="email" id="su-email" placeholder="AAAA/MM/DD" required="">
                   <div class="invalid-feedback">Porfavor, ingrese una fecha válida.</div>
                 </div>
                 <div class="form-group">
-                  <label for="su-email">Correo electrónico</label>
+                  <label class="colorFuente" for="su-email">Correo electrónico</label>
                   <input class="form-control" type="email" id="su-email" placeholder="tuCorreo@ejemplo.com" required="">
                   <div class="invalid-feedback">Porfavor, ingrese un correo válido.</div>
                 </div>
                 <div class="form-group contra">
-                  <label for="su-password">Contraseña</label>
+                  <label class="colorFuente" for="su-password">Contraseña</label>
                   <div class="password-toggle">
                     <input class="form-control" type="password" id="su-password" required="" placeholder="********">
                     <!-- <label class="password-toggle-btn">
@@ -151,32 +161,32 @@ class CustomHeader extends HTMLElement {
                   </div>
                 </div>
                 <div class="form-group">
-                  <label for="su-password-confirm">Confirmar contraseña</label>
+                  <label class="colorFuente" for="su-password-confirm">Confirmar contraseña</label>
                   <div class="password-toggle">
                     <input class="form-control" type="password" id="su-password-confirm" required="" placeholder="********">
                   </div>
                 </div>
                 <div class="form-row">
                 <div class="form-group col-lg-4 col-12 pl-2">
-                  <label for="su-name">País</label>
+                  <label class="colorFuente" for="su-name">País</label>
                   <input class="form-control" type="text" id="su-name" placeholder="País" required="">
                 </div>
                 <div class="form-group col-lg-4 col-12 pl-2">
-                  <label for="su-name">Ciudad</label>
+                  <label class="colorFuente" for="su-name">Ciudad</label>
                   <input class="form-control" type="text" id="su-name" placeholder="Ciudad" required="">
                 </div>
                 <div class="form-group col-lg-4 col-12 pl-2">
-                  <label for="su-name">Provincia</label>
+                  <label class="colorFuente" for="su-name">Provincia</label>
                   <input class="form-control" type="text" id="su-name" placeholder="Provincia" required="">
                 </div>
                 </div>
                 <div class="form-group col-lg-12 col-12 pl-0 pr-0">
-                  <label for="su-name">Direccion</label>
+                  <label class="colorFuente" for="su-name">Direccion</label>
                   <input class="form-control" type="text" id="su-name" placeholder="Direccion" required="">
                   <div class="invalid-feedback">Porfavor, ingrese una dirección válida.</div>
                 </div>
                 <div class="text-center p-2">
-                  <button class="btn btn-outline-dark btn-lg" type="submit">Registráte</button>
+                  <button class="botonLogSign btn-lg" type="submit">Registráte</button>
                 </div>
               </form>
           </div>
@@ -186,6 +196,7 @@ class CustomHeader extends HTMLElement {
     </div>
 
     <!-- CONTACTO -->
+    
     <div class="modal fade" id="contacto" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-body animated slideInDown">
         <div class="modal-dialog modal-dialog-centered" role="document">
